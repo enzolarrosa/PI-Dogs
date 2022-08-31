78056e8ab34d98d0e8fc6a86ad6a89ff5bb09de2
Nicolas Larrosa <enzolarrosa@gmail.com>
2022-08-31 18:35:26 -0300

    solucionado

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -20,11 +20,9 @@ module.exports = (sequelize) => {
     },
     height: {
       type: DataTypes.STRING,
-      allowNull: false ,
     },
     weight: {
       type: DataTypes.STRING,
-      allowNull: false ,
     },
     lifeSpan: {
       type: DataTypes.STRING,
--- api/src/routes/Dog.js
+++ api/src/routes/Dog.js
@@ -3,28 +3,34 @@ const router = Router();
 const { Dog, Temperament } = require("../db");
 
 router.post("/", async (req, res) => {
-  const { name, img, height1, height2, weight1, weight2, lifeSpan1, lifeSpan2, temp } = req.body;
-  const height= [...height1,height2]
-  const weight= [...weight1,weight2]
-  const lifeSpan= [...lifeSpan1,lifeSpan2]
-  let h = height[0] + ' - ' + height[1]
-  let w = weight[0] + ' - ' + weight[1]
-  let l = lifeSpan[0] + ' - ' + lifeSpan[1] + ' years'
-  const neew = await Dog.create({
-    name,
-    img,
-    h,
-    w,
-    l,
-  });
-  temp.map(async (e) => {
-    let post = await Temperament.findAll({
-      where: { name: e.toLowerCase() },
+  try {
+    const { name, img, height1, height2, weight1, weight2, lifeSpan1, lifeSpan2, temperament } = req.body;
+    const height= height1.concat(height2)
+    const weight= weight1.concat(weight2)
+    const lifeSpan= lifeSpan1.concat(lifeSpan2)
+    console.log(height)
+    let h = height[0] + ' - ' + height[1]
+    let w = weight[0] + ' - ' + weight[1]
+    let l = lifeSpan[0] + ' - ' + lifeSpan[1] + ' years'
+    console.log(h)
+    const neew = await Dog.create({
+      name,
+      img,
+      height: h,
+      weight: w,
+      lifeSpan: l,
     });
-    await neew.addTemperament(post);
-  });
-
-  res.send("Finish");
+    temperament.map(async (e) => {
+      let post = await Temperament.findAll({
+        where: { name: e.toLowerCase() },
+      });
+      await neew.addTemperament(post);
+    });
+  
+    res.send("Finish");
+  } catch (error) {
+    console.log(error.message)
+  }
 });
 
 module.exports = router;
