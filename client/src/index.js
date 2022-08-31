554acfcf9a4bb89fe3ebea07b5a15ec3e78d3430
Nicolas Larrosa <enzolarrosa@gmail.com>
2022-08-31 02:57:48 -0300

    finish

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,11 @@ import store from './store'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios'
+import axios from 'axios';
 import dotenv from 'dotenv'
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store} >
