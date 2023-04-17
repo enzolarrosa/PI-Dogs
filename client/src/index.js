34714dca3cffdd25ad7699ec3d79531e9db66e54
Nicolas Larrosa <enzolarrosa@gmail.com>
2023-04-17 01:50:03 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 // axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.baseURL= "https://pi-dogs-production-19be.up.railway.app"
+axios.defaults.baseURL= "https://pi-dogs-production-e6ba.up.railway.app"
 
 ReactDOM.render(
   <Provider store={store} >
